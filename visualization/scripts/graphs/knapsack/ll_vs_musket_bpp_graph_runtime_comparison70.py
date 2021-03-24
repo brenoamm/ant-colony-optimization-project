583229fec11163891f9f57cf11db8be6faadaf4e
breno menezes <breno.menezes@uni-muenster.de>
2021-03-24 09:25:06 +0100

    new mkp quadro results v100

---
+++ visualization/scripts/graphs/knapsack/ll_vs_musket_bpp_graph_runtime_comparison70.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 11:58:45 2020
+
+@author: bamm
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+ma_path = '../bpp_output_data/musket/70paking.dat'
+ll_path = '../bpp_output_data/ll/256_v100_complete_bpp.out'
+
+#extract data from files
+Musket1024Average = [0,0,0,0,0,0]
+Musket2048Average = [0,0,0,0,0,0]
+Musket4096Average = [0,0,0,0,0,0]
+Musket8192Average = [0,0,0,0,0,0]
+
+MusketAverage = [Musket1024Average, Musket2048Average, Musket4096Average, Musket8192Average]
+
+ll1024Average = [0,0,0,0,0,0]
+ll2048Average = [0,0,0,0,0,0]
+ll4096Average = [0,0,0,0,0,0]
+ll8192Average = [0,0,0,0,0,0]
+
+lowlevelAverage = [ll1024Average, ll2048Average, ll4096Average, ll8192Average]
+
+#iterate over files
+for f in range(6):
+    
+    file_path_str = ll_path
+    ma_path_str = ma_path
+    
+    ll_data = pd.read_csv(file_path_str, delimiter=',', header=None)
+    ma_data = pd.read_csv(ma_path_str, delimiter=';', header=None)
+    
+    for x in range(4):
+        ma = 0
+        lla = 0
+        
+        for y in range(5):
+            lla = lla + ll_data.at[((f*20)+(x*5)+y), 3]            
+            
+        for y in range(10):
+            ma = ma + ma_data.at[((f*40)+(x*10)+y), 19]
+
+
+        ma = ma/10
+        lla = lla/5
+        
+        MusketAverage[x][f] = ma
+        lowlevelAverage[x][f] = lla
+
+#define size
+fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, figsize=(15, 10))
+
+# X-Achsis
+years = [0,1,2,3,4,5]
+ind = np.arange(len(years))
+width = 0.35         # the width of the bars
+
+BrenoBars_1024 = axes[0, 0].bar(ind, ll1024Average, width, bottom=0, color = '#A60628')
+BrenoBars_2048 = axes[1, 0].bar(ind, ll2048Average, width, bottom=0, color = '#A60628')
+BrenoBars_4096 = axes[0, 1].bar(ind, ll4096Average, width, bottom=0, color = '#A60628')
+BrenoBars_8192 = axes[1, 1].bar(ind, ll8192Average, width, bottom=0, color = '#A60628')
+
+MusketBars_1024 = axes[0, 0].bar(ind+width, Musket1024Average, width,  bottom=0, color = '#348ABD')
+MusketBars_2048 = axes[1, 0].bar(ind+width, Musket2048Average, width,  bottom=0, color = '#348ABD')
+MusketBars_4096 = axes[0, 1].bar(ind+width, Musket4096Average, width,  bottom=0, color = '#348ABD')
+MusketBars_8192 = axes[1, 1].bar(ind+width, Musket8192Average, width,  bottom=0, color = '#348ABD')
+
+#MusketBars_1024 = axes[0, 0].bar(ind+width, ll1024Average, width,  bottom=0, color = '#348ABD')
+#MusketBars_2048 = axes[1, 0].bar(ind+width, ll2048Average, width,  bottom=0, color = '#348ABD')
+#MusketBars_4096 = axes[0, 1].bar(ind+width, ll4096Average, width,  bottom=0, color = '#348ABD')
+#MusketBars_8192 = axes[1, 1].bar(ind+width, ll8192Average, width,  bottom=0, color = '#348ABD')
+
+#BrenoBars_1024 = axes[0, 0].bar(ind, Musket1024Average, width, bottom=0, color = '#A60628')
+#BrenoBars_2048 = axes[1, 0].bar(ind, Musket2048Average, width, bottom=0, color = '#A60628')
+#BrenoBars_4096 = axes[0, 1].bar(ind, Musket4096Average, width, bottom=0, color = '#A60628')
+#BrenoBars_8192 = axes[1, 1].bar(ind, Musket8192Average, width, bottom=0, color = '#A60628')
+
+axes[0, 0].set_title('1024 Ants')
+axes[1, 0].set_title('2048 Ants')
+axes[0, 1].set_title('4096 Ants')
+axes[1, 1].set_title('8192 Ants')
+
+axes[0, 0].set_ylabel('seconds')
+axes[1, 0].set_ylabel('seconds')
+axes[0, 1].set_ylabel('seconds')
+axes[1, 1].set_ylabel('seconds')
+
+axes[0, 0].set_xlabel('problem')
+axes[1, 0].set_xlabel('problem')
+axes[0, 1].set_xlabel('problem')
+axes[1, 1].set_xlabel('problem')
+
+axes[0, 0].set_xticks(ind + width / 2)
+axes[0, 0].set_xticklabels(years)
+
+#for ax in axes.flat:
+    #ax.label_outer()
+#for ax in fig.get_axes():
+ #   ax.label_outer()
+axes[0, 0].legend((BrenoBars_1024[0],MusketBars_1024[0]), ('Low-level','Musket'))
+axes[0, 0].autoscale_view()
+
+plt.show()
